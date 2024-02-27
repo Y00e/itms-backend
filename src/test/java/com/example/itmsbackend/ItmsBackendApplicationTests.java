451cd1c41af2e80d6e92af73f37f99c9055dfeb2
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2024-02-27 13:15:33 +0100

    Add files via upload

---
+++ src/test/java/com/example/itmsbackend/ItmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
