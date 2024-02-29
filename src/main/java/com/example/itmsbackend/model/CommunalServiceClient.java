1b45068314c5842da3611eea1f344a8b7e67c9cd
YoussefMu23 <dembouz719@gmail.com>
2024-02-29 10:26:40 +0100

    created CommunalServiceClient with RestTemplate

--- src/main/java/com/example/itmsbackend/model/CommunalServiceClient.java
+++ src/main/java/com/example/itmsbackend/model/CommunalServiceClient.java
@@ -6,13 +6,13 @@ import org.springframework.web.client.RestTemplate;
 
 public class CommunalServiceClient {
 
-    private String communalServiceUrl = "";
+    private String communalServiceUrl = "https://transport-service.azurewebsites.net/api";
 
     private RestTemplate restTemplate = new RestTemplate();
 
 
     public CommunalTransportDto getRoute(Long id) {
-        ResponseEntity<CommunalTransportDto> response = restTemplate.getForEntity(communalServiceUrl + "/communal-transport/" + id, CommunalTransportDto.class);
+        ResponseEntity<CommunalTransportDto> response = restTemplate.getForEntity(communalServiceUrl + "/communal-transport" + id, CommunalTransportDto.class);
         return response.getBody();
     }
 
