2474a07cac064c3703423ca00e5dc844ddfd42f7
YoussefMu23 <dembouz719@gmail.com>
2024-02-29 10:58:01 +0100

    created model combined_routes

---
+++ src/main/java/com/example/itmsbackend/model/CombinedRoute.java
@@ -0,0 +1,22 @@
+package com.example.itmsbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "combined_routes")
+public class CombinedRoute {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String start;
+    private String end;
+
+}
