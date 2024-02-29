fc73359887d5107e8fbe4b882730912963ab69ee
YoussefMu23 <dembouz719@gmail.com>
2024-02-29 11:16:33 +0100

    added @Service

--- src/main/java/com/example/itmsbackend/model/CommunalServiceClient.java
+++ src/main/java/com/example/itmsbackend/model/CommunalServiceClient.java
@@ -2,8 +2,10 @@ package com.example.itmsbackend.model;
 
 import com.example.itmsbackend.dto.CommunalTransportDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class CommunalServiceClient {
 
     private String communalServiceUrl = "https://transport-service.azurewebsites.net/api";
