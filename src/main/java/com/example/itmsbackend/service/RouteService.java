42e7e020485f888846dde75eb8b7a3bd3d172d6d
YoussefMu23 <dembouz719@gmail.com>
2024-02-29 20:18:16 +0100

    create method for createCombinedRoute & getAllCombinedRoutes

--- src/main/java/com/example/itmsbackend/service/RouteService.java
+++ src/main/java/com/example/itmsbackend/service/RouteService.java
@@ -1,7 +1,11 @@
 package com.example.itmsbackend.service;
 
+import com.example.itmsbackend.dto.CommunalTransportDto;
+import com.example.itmsbackend.model.CombinedRoute;
+import com.example.itmsbackend.model.CommunalServiceClient;
 import com.example.itmsbackend.model.FavoriteRoute;
 import com.example.itmsbackend.model.Route;
+import com.example.itmsbackend.repository.CombinedRouteRepository;
 import com.example.itmsbackend.repository.FavoriteRouteRepository;
 import com.example.itmsbackend.repository.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,13 @@ public class RouteService {
 
     @Autowired
     private FavoriteRouteRepository favoriteRouteRepository;
+
+
+    @Autowired
+    private CommunalServiceClient communalServiceClient;
+
+    @Autowired
+    private CombinedRouteRepository combinedRouteRepository;
 
 
     public List<Route> getAll() {
@@ -58,4 +69,18 @@ public class RouteService {
     }
 
 
+    public CombinedRoute createCombinedRoute(Long communalTransportDtoId, Long routeId) {
+        CommunalTransportDto communalTransportDto = communalServiceClient.getRoute(communalTransportDtoId);
+        Route route = routeRepository.findById(routeId).orElseThrow(() -> new RuntimeException("CommunalTransport not found with id " + routeId));
+
+        CombinedRoute combinedRoute = new CombinedRoute();
+        combinedRoute.setStartRoute(route.getStart());
+        combinedRoute.setEndCommunalTransport(communalTransportDto.getToPlace());
+        return combinedRouteRepository.save(combinedRoute);
+    }
+
+    public List<CombinedRoute> getAllCombinedRoutes() {
+        return combinedRouteRepository.findAll();
+
+    }
 }
