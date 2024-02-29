e3ea2e20961f3f27835b033eccd10df454b214a8
YoussefMu23 <dembouz719@gmail.com>
2024-02-29 11:16:14 +0100

    created CombinedRouteRepository

---
+++ src/main/java/com/example/itmsbackend/repository/CombinedRouteRepository.java
@@ -0,0 +1,8 @@
+package com.example.itmsbackend.repository;
+
+import com.example.itmsbackend.model.CombinedRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CombinedRouteRepository extends JpaRepository<CombinedRoute, Long > {
+
+}
